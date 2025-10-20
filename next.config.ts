b454e74216593325451002d976c249a40a79c098
Abram2173 <127432561+Abram2173@users.noreply.github.com>
2025-10-20 14:39:19 -0600

    Incluir archivos estáticos de out/ para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',  // Exporta como sitio estático
-  basePath: '/doc-agil',  // Nombre de tu repo
-  assetPrefix: '/doc-agil/',  // Para assets como CSS e imágenes
+  output: 'export',
+  basePath: '/doc-agil',
+  assetPrefix: '/doc-agil/',
   images: {
-    unoptimized: true  // Necesario para sitios estáticos
+    unoptimized: true
   },
-  trailingSlash: true  // Opcional, para URLs consistentes
+  trailingSlash: true
 };
 
 export default nextConfig;
