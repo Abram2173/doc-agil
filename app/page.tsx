6d9f1e30a7e7c086a32a28dfef66b8abdc21cf87
Abram2173 <127432561+Abram2173@users.noreply.github.com>
2025-10-20 10:59:59 -0600

    agregar el principal ventana 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
   // Redirige inmediatamente a /login
   redirect("/dashboard");
 
